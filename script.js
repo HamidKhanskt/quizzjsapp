1115720a4695ce388cb0204e41fe616416d9469c
hamid khan <hk339954@gmail.com>
2024-06-15 13:35:51 +0500

    updated file

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ const quizData = [
 
 let currentQuiz = 0;
 let score = 0;
+let timer;
+const timePerQuestion = 15; // 15 seconds per question
 
 const questionEl = document.getElementById('question');
 const answerEls = document.querySelectorAll('.answer');
@@ -59,6 +61,7 @@ const option2 = document.getElementById('option-2');
 const option3 = document.getElementById('option-3');
 const option4 = document.getElementById('option-4');
 const submitBtn = document.getElementById('submit');
+const timerEl = document.getElementById('time');
 
 function loadQuiz() {
     deselectAnswers();
@@ -70,6 +73,8 @@ function loadQuiz() {
     option2.innerText = currentQuizData.b;
     option3.innerText = currentQuizData.c;
     option4.innerText = currentQuizData.d;
+
+    resetTimer();
 }
 
 function deselectAnswers() {
@@ -86,28 +91,43 @@ function getSelected() {
     return answer;
 }
 
-loadQuiz();
+function resetTimer() {
+    clearInterval(timer);
+    let timeLeft = timePerQuestion;
+    timerEl.innerText = timeLeft;
 
-submitBtn.addEventListener('click', () => {
+    timer = setInterval(() => {
+        timeLeft--;
+        timerEl.innerText = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            moveToNextQuestion();
+        }
+    }, 1000);
+}
+
+function moveToNextQuestion() {
     const answer = getSelected();
 
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-
-        currentQuiz++;
-
-        if (currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            document.getElementById('quiz').innerHTML = `
-                <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
-                <button class="reload-button" onclick="location.reload()">Reload</button>
-            `;
-        }
-    } else {
-        alert('Please select an answer before submitting!');
+    if (answer && answer === quizData[currentQuiz].correct) {
+        score++;
     }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        document.getElementById('quiz').innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
+            <button class="reload-button" onclick="location.reload()">Reload</button>
+        `;
+    }
+}
+
+submitBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    moveToNextQuestion();
 });
 
+loadQuiz();
